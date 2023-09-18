e198c18509f600f46b9261af793c37bb0141844e
Masahiro Oono <words.ssp@gmail.com>
2023-09-19 06:29:43 +0900

    Change loadText() to pass a file name

--- main.c
+++ main.c
@@ -13,22 +13,28 @@
 
 typedef unsigned char *String;
 
-void loadText(int argc, const char **argv, String text, int size)
+int loadText(String path, String text, int size)
 {
-  if (argc < 2) {
-    printf("Usage: %s program-file\n", argv[0]);
-    exit(1);
-  }
+  unsigned char buf[1000];
 
-  FILE *fp = fopen(argv[1], "rt");
+  int startPos = path[0] == '"'; // ダブルクォートがあれば外す
+  int i = 0;
+  while (path[startPos + i] != 0 && path[startPos + i] != '"') {
+    buf[i] = path[startPos + i];
+    ++i;
+  }
+  buf[i] = 0;
+
+  FILE *fp = fopen(buf, "rt");
   if (fp == NULL) {
-    printf("Failed to open %s\n", argv[1]);
-    exit(1);
+    printf("Failed to open %s\n", path);
+    return 1;
   }
 
   int nItems = fread(text, 1, size - 1, fp);
   fclose(fp);
   text[nItems] = 0;
+  return 0;
 }
 
 #define MAX_TOKEN_CODE 1000 // トークンコードの最大値
@@ -109,8 +115,14 @@ int tc[10000]; // トークンコードを格納する
 
 int main(int argc, const char **argv)
 {
+  if (argc < 2) {
+    printf("Usage: %s program-file\n", argv[0]);
+    exit(1);
+  }
+
   unsigned char text[10000];
-  loadText(argc, argv, text, 10000);
+  if (loadText((String) argv[1], text, 10000) != 0)
+    exit(1);
 
   int equal     = getTokenCode("==", 2);
   int notEq     = getTokenCode("!=", 2);
